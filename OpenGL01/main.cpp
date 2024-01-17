cbce70b91bc3e9d7f0e0f9814d2d557d30755f06
Yuanzidan081 <1579417227@qq.com>
2024-01-17 14:38:33 +0800

    OpenGL01 commit

---
+++ OpenGL01/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+//============================ Function ============================
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+//============================ main ============================
+int main()
+{
+    //实例化GLFW窗口
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //创建一个窗口对象
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    //初始化GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    //渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        //输入
+        processInput(window);
+
+        // 渲染指令-类似黑板的颜色
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //交换前后帧
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Reference:
+
+[OpenGL入门](https://www.bilibili.com/video/BV1sb4y1J7YT/?spm_id_from=333.788&vd_source=e4ce9f685d3cd2277925371c4c55f27f)
+
+[learnopengl-cn](https://learnopengl-cn.github.io/)
